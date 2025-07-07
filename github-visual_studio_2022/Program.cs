93764ca357ca2096cb39798278d8f996baa769e9
figulazmi <azmi.codes@gmail.com>
2025-07-07 11:51:30 +0700

    Add project files.

---
+++ github-visual_studio_2022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace github_visual_studio_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
